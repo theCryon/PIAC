81d3eb661a6c2cea86606bd455ab1d5afc4eb7dc
theCryon <66066260+theCryon@users.noreply.github.com>
2021-06-13 16:03:32 +0200

    lab3

--- run.py
+++ run.py
@@ -1,19 +1,22 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
+from flask_mail import Mail, Message
 from flask import abort, redirect, url_for, make_response
 
 app = Flask(__name__)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'mkrajnik225@gmail.com'
+app.config['MAIL_PASSWORD'] = 'g2tra7!2CEhL'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 
 @app.route('/aboutme')
@@ -26,26 +29,47 @@ def gallery():
     return render_template('gallery.html')
 
 
-@app.route('/error_denied')
-def error_denied():
-    abort(401)
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
-@app.route('/error_internal')
-def error_internal():
-    return render_template('template.html', name='ERROR 505'), 505
+@app.route("/send_message", methods=['GET', 'POST'])
+def send_message():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        msg = request.form['message']
 
+        message = Message(name, sender="mkrajnik225@gmail.com", recipients=[email])
 
-@app.route('/error_not_found')
-def error_not_found():
-    response = make_response(render_template('template.html', name='ERROR 404'), 404)
-    response.headers['X-Something'] = 'A value'
-    return response
+        message.body = msg
 
+        mail.send(message)
+        success = "Message sent"
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
+        return render_template("contact.html", success=success)
+
+# @app.route('/error_denied')
+# def error_denied():
+#     abort(401)
+#
+#
+# @app.route('/error_internal')
+# def error_internal():
+#     return render_template('template.html', name='ERROR 505'), 505
+#
+#
+# @app.route('/error_not_found')
+# def error_not_found():
+#     response = make_response(render_template('template.html', name='ERROR 404'), 404)
+#     response.headers['X-Something'] = 'A value'
+#     return response
+#
+#
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
